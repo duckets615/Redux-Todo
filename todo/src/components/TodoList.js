a8f2a5e6da678a5906afa70d41b89228f4f14a4d
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-03-04 18:42:50 -0500

    todolist component

--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -1,40 +1,53 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
 import Todo from './Todo';
-import { addTodo, toggleCompleted } from '../actions';
+import {handleInput, todoAdd, todoComplete, todoDelete} from '../Actions'; 
 
+const TodoList = props => {
 
-class TodoList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputValue: ''
+    const addSubmit = e => {
+        e.preventDefault();
+        props.todoAdd();
     }
-  }
 
-  onChange = event => {
-    this.setState({inputValue: event.target.value})
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Todo List</h1>
-        <form>
-          <input placeholder="Todo" value={this.state.inputValue} onChange={this.onChange}/>
-          <button type='submit' onClick={(event)=> { event.preventDefault(); this.props.addTodo(this.state.inputValue)}}>Add Todo</button>
-        </form> 
-        <ul>
-          <Todo todos={this.props.todos} toggleCompleted={this.props.toggleCompleted} />   
-        </ul>
-      </div>
+    return  (
+        <section className="todo-list">
+            <div className="container">
+                <div className="todos">
+                    {props.todos.map(
+                        todo => <Todo key={todo.id} completed={todo.completed} complete={props.todoComplete} id={todo.id} text={todo.text}/>
+                    )}
+                </div>
+                <div className="todo-form">
+                    <form onSubmit={addSubmit}>
+                        <div className="new-todo container">
+                            <label htmlFor="new-todo" className={`${props.currentlyTyping ? "is-typing" : ""}`}>Add task here</label>
+                            <input 
+                            type="text" 
+                            name="new-todo" 
+                            id="new-todo" 
+                            className={`${props.currentlyTyping ? "typing" : ""}`} 
+                            value={props.currentInputText} 
+                            onChange={(e) => props.handleInput(e.currentTarget.value, e.currentTarget)} 
+                            onFocus={(e) => props.handleInput(e.currentTarget.value, e.currentTarget)} 
+                            onBlur={(e) => props.handleInput(e.currentTarget.value, e.currentTarget)} 
+                            />
+                        </div>
+                        <div className="add-todo todo-button" onClick={addSubmit}>Add Task</div>
+                    </form>
+                        <div className="remove-todo todo-button" onClick={() => props.todoDelete()}>Remove Tasks</div>
+                </div>
+            </div>
+        </section>
     )
-  }
 }
 
-const mapStateToProps = state => ({
-  todos: state.todos
-});
+const connectMyProps = state => (
+    {   
+        todos: state.todos,
+        currentInputText: state.currentInputText,
+        currentlyTyping: state.currentlyTyping
+    }
+)
 
-export default connect(mapStateToProps,{ addTodo, toggleCompleted })(TodoList);
+export default connect(connectMyProps, { handleInput, todoAdd, todoComplete, todoDelete })(TodoList);
